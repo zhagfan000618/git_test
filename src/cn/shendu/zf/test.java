e0bf35d45a56105d0f0d67a8696406891928c7c5
zhangfan <zhangfan@zhang.cn>
2019-08-02 19:00:55 +0800

    添加内容

--- src/cn/shendu/zf/test.java
+++ src/cn/shendu/zf/test.java
@@ -4,4 +4,8 @@ public class test {
     public static void main(String[] args) {
         System.out.println("aaaaaaaaaaaaaaaaaaaaa");
     }
+    public void   test1()  {
+        int i=0;
+        i+=10;
+    }
 }
